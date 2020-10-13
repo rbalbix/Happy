de95dade868d2da13e28aec9109864d9647d0a5b
Ricardo Balbi <rbalbi@gmail.com>
2020-10-13 10:02:56 -0300

    chore(config): add initial backend config

---
+++ backend/src/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+
+// Import routes
+import BaseRouter from './routes';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.disable('x-powered-by');
+
+app.use(BaseRouter);
+
+const PORT = process.env.PORT || 3333;
+app.listen(PORT, () => {
+  console.log('@port', PORT);
+});
